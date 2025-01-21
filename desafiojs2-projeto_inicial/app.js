54dc2c1ff9461d29adb73f498b6b681172cafd8d
Rafael Maciel <rafaelmaciels@outlook.com>
2025-01-20 23:22:28 -0300

    Desafio Hora da Pratica
    Aprendemos a melhorar o código existente, tornando-o mais eficiente e legível. Neste caso, refatoramos a função que realiza o sorteio do número secreto, tornando-a mais robusta e garantindo que o número sorteado não seja repetido;

---
+++ desafiojs2-projeto_inicial/app.js
@@ -0,0 +1,17 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+    let listaGenerica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos:
+// 'JavaScript','C','C++', 'Kotlin' e 'Python', depois adicione a lista as linguagens 'Java', 'Ruby', 'GoLang'. usando a função push. 
+    let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+    linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+    console.log(linguagensDeProgramacao); 
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+    let nomes = ['Lucas', 'João', 'Maria'];
+// Exiba no console apenas o primeiro elemento.    
+    console.log(nomes[0]);    
+// Exiba no console apenas o segundo elemento.
+    console.log(nomes[1]);
+// Exiba no console apenas o terceiro elemento.
+    console.log(nomes[2]);      
